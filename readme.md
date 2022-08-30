853e39102bce3f1d16d3b960804b2d3f009690b7
Matheus Spinosa <spinosa665@gmail.com>
2022-08-30 16:41:24 -0300

    first commit

---
+++ readme.md
@@ -0,0 +1,12 @@
+/* Function */
+Function name: BkodeCurrencyFormat
+values: {
+  number?: number;
+  decimal?: "," | "." | string;
+  thousands?: "," | "." | string;
+  symbol?: "R$" | "$" | "¥" | string;
+  decimalNumbers?: number;
+}
+response: string
+
+OBS: All fields are customized within their respective types, some have example types but accept almost any type of value as the symbol field
---
+++ src/index.ts
@@ -0,0 +1,50 @@
+
+interface IProps {
+  number?: number;
+  decimal?: "," | "." | string;
+  thousands?: "," | "." | string;
+  symbol?: "R$" | "$" | "¥" | string;
+  decimalNumbers?: number;
+}
+
+function BkodeCurrencyFormat(
+  {
+    number,
+    decimal,
+    thousands,
+    symbol,
+    decimalNumbers,
+  }: IProps
+): string {
+  number  = number || 0,
+  decimal  = decimal || ",",
+  thousands  = thousands || ".",
+  symbol  = symbol || "",
+  decimalNumbers  = decimalNumbers || 2
+  const numberString = number.toString();
+
+  if (!numberString.match(".")) {
+    return `${numberString}.00`;
+  }
+
+  let numberSplit = numberString.split(".");
+
+  if (numberSplit.length != 2) {
+    numberSplit = number.toFixed(decimalNumbers).split(".");
+
+    if (numberSplit.length != 2) {
+      return "Huston we have a problem!";
+    }
+  }
+
+  const integer = numberSplit[0].replace(/(.)(?=(\d{3})+$)/g, `$1${thousands}`);
+
+  let decimalValue = numberSplit[1].substring(0, decimalNumbers);
+
+  decimalValue = decimalValue.padEnd(decimalNumbers, "0");
+
+  return `${symbol} ${integer}${decimal}${decimalValue}`;
+}
+
+export { BkodeCurrencyFormat };
+
